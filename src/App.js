be2922ac1e21b1e5a2da0becfb05990548efe687
Serhii <serhiymysienko@gmail.com>
2021-05-11 22:16:16 +0300

    start of the project. add dependancy. connect materialize. few pages made.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React, { useEffect, Fragment } from 'react';
+
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 function App() {
+  useEffect(() => {
+    // initialize of materialize JS
+    M.AutoInit();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <SearchBar></SearchBar>
+      <div className='container'>
+        {' '}
+        <AddBtn></AddBtn>
+        <AddLogModal></AddLogModal>
+        <Logs></Logs>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/logs/AddLogModal.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const AddLogModal = () => {
+  const [message, setMessage] = useState('');
+  const [attention, setAttention] = useState(false);
+  const [tech, setTech] = useState('');
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (message === '' || tech === '') {
+      M.toast({ html: 'Please enter all fields' });
+    } else {
+      console.log(message, tech, attention);
+    }
+  };
+
+  //  id of returned div MUST be equal to href of clicked btn !!!
+  return (
+    <div className='modal' id='add-log-modal' style={modalStyle}>
+      <div className='modal-content'>
+        <h4>Enter system Log</h4>
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              name='message'
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+            />
+            <label htmlFor='message' className='active'>
+              Log Message
+            </label>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='input-field'>
+            <select
+              name='tech'
+              value={tech}
+              className='browser-default'
+              onChange={e => setTech(e.target.value)}
+            >
+              <option value='' disabled>
+                {' '}
+                Select Tech{' '}
+              </option>
+              <option value='John Doe'>John Doe</option>
+              <option value='Sam Smith'>Sam Smith</option>
+              <option value='Sara'>Sara</option>
+            </select>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='input-field'>
+            <p>
+              <label>
+                <input
+                  type='checkbox'
+                  className='filled-in'
+                  checked={attention}
+                  value={attention}
+                  onChange={e => setAttention(!attention)}
+                />
+                <span>Needs attention</span>
+              </label>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='modal-footer'>
+        <a
+          href='#!'
+          onClick={onSubmit}
+          className='waves-effect waves-green btn blue'
+        >
+          Enter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const modalStyle = {
+  width: '75%',
+  height: '75%',
+};
+export default AddLogModal;
